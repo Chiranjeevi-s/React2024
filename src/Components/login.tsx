40a457c3d91b531a00c167b356a5b8eefdc86001
chiranjeevi <chirusheelvanth@gmail.com>
2024-01-14 20:08:33 +0530

    commit_two

--- src/Components/login.tsx
+++ src/Components/login.tsx
@@ -30,6 +30,32 @@ const Login: React.FunctionComponent<ILoginProps> = () => {
         gap: "10px",
       }}
     >
+            <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        Enter Name or Email :{" "}
+        <TextField
+          id="outlined-basic"
+          label="Name / Email"
+          variant="outlined"
+        />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        Password :{" "}
+        <TextField id="outlined-basic" label="Password" variant="outlined" />
+      </Box>
+
+      <Button variant="contained">Login</Button>
 
     </div>
   );
