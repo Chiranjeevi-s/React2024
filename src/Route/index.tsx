fcde963aa713a0148f7bc1a24ef8cc8f0189eacb
chiranjeevi <chirusheelvanth@gmail.com>
2024-01-14 23:21:13 +0530

    Debounce --Complete

--- src/Route/index.tsx
+++ src/Route/index.tsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../Components/login";
 import LazyLoading from "../Components/Lazy Loading Components/lazyloading";
+import Debounce from "../Components/debounce";
 
 const COMPO_ROUTER = [
-    {
-        path:'',
-        element:<Login />
-    },
-    {
-        path:'/lazy',
-        element:<LazyLoading />
-    }
-]
+  {
+    path: "",
+    element: <Login />,
+  },
+  {
+    path: "/lazy",
+    element: <LazyLoading />,
+  },
+  {
+    path: "/debounce",
+    element: <Debounce />,
+  },
+];
 
-const route = createBrowserRouter([...COMPO_ROUTER])
+const route = createBrowserRouter([...COMPO_ROUTER]);
 
-export {route}
+export { route };
