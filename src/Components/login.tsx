805e82ecedd38a58431e8b5c719e4271139ea2c0
chiranjeevi <chirusheelvanth@gmail.com>
2024-01-14 10:32:40 +0530

    Axios Setup --complete

--- src/Components/login.tsx
+++ src/Components/login.tsx
@@ -1,6 +1,22 @@
+import React from "react";
+import axiosInstance from "../axios";
+
 interface ILoginProps {}
 
 const Login: React.FunctionComponent<ILoginProps> = () => {
+  const loadIntialValues = async () => {
+    try {
+      let { data } = await axiosInstance.get(`https://dummyjson.com/produc`);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  React.useEffect(() => {
+    loadIntialValues();
+  }, []);
+
   return <>Login</>;
 };
 
